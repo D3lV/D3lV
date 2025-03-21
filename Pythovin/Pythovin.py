dbf1fc4fd62b3cdb5fc8729011920829f7a3a480
D3lV <delvinia09@gmail.com>
2025-03-21 10:15:22 -0400

    Add files via upload

---
+++ Pythovin/Pythovin.py
@@ -0,0 +1,16 @@
+
+import webbrowser
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file_explorer():
+    root = tk.Tk()
+    root.withdraw()  # Oculta la ventana principal
+    root.attributes('-topmost', True)  # Asegura que la ventana esté al frente
+    folder_selected = filedialog.askdirectory(title="Selecciona una carpeta")
+    if folder_selected:
+        os.startfile(folder_selected)
+
+open_file_explorer()
+webbrowser.open("https://github.com/D3lV/D3lV")
